df1cdbdbc9256cb56fc58236165701521d78972f
Luiza Leao <luiza.leao96@gmail.com>
2023-11-16 15:59:55 -0500

    Initial commit. Worked on it with @SofPan and @D-Dinara.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const mapAndJoinLetters = (arr) => {
+  return arr.map(ls => ls.join(''));
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) return false;
+  const horizontalJoin = mapAndJoinLetters(letters);
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  let reveresedMatrix = [];
+
+  for (let i = 0; i < letters[0].length; i++) {
+    reveresedMatrix[i] = [];
+    for (let j = 0; j < letters.length; j++) {
+      reveresedMatrix[i].push(letters[j][i]);
+    }
+  }
+
+  const verticalJoin = mapAndJoinLetters(reveresedMatrix);
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
